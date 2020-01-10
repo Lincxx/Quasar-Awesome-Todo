909a7692d9b3572155802af45c6468ddbb683a88
Lincxx <bossco28@gmail.com>
2020-01-10 06:25:33 -0500

    vue looping

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,9 +1,44 @@
 <template>
-  <q-page padding></q-page>
+  <q-page padding>
+    <ul>
+      <li v-for="(task, index) in tasks" :key="index">
+        <div>{{ task.name }}</div>
+        <small>{{ task.dueDate }} @ {{ task.dueTime }}</small>
+        <button @click="deleteTask(index)">X</button>
+      </li>
+    </ul>
+  </q-page>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      tasks: [
+        {
+          name: "Go to shop",
+          dueDate: "5/12/2019",
+          dueTime: "18:34"
+        },
+        {
+          name: "Get Bananas",
+          dueDate: "5/15/2019",
+          dueTime: "1:45"
+        },
+        {
+          name: "Get appples",
+          dueDate: "6/1/2019",
+          dueTime: "8:34"
+        }
+      ]
+    };
+  },
+  methods: {
+    deleteTask(index) {
+      this.tasks.splice(index, 1);
+    }
+  }
+};
 </script>
 
 <style>
